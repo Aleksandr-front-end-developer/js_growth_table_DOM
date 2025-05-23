d01389c989e51682a3bb466eb37c73d71db544f8
Aleksandr-front-end-developer <oleksandr.danyliuk.88@gmail.com>
2025-05-23 22:14:47 +0300

    have done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,89 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('.field tbody');
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+let rowsCount = tbody.children.length;
+let columnsCount = tbody.querySelector('tr').children.length;
+
+document.addEventListener('click', handleClick);
+
+function handleClick(e) {
+  if (e.target.closest('.button') === appendRow) {
+    doingTable('appendRow');
+  }
+
+  if (e.target.closest('.button') === removeRow) {
+    doingTable('removeRow');
+  }
+
+  if (e.target.closest('.button') === appendColumn) {
+    doingTable('appendColumn');
+  }
+
+  if (e.target.closest('.button') === removeColumn) {
+    doingTable('removeColumn');
+  }
+}
+
+function doingTable(change) {
+  const row = tbody.querySelector('tr');
+  const column = tbody.querySelectorAll('tr');
+
+  if (change === 'appendRow') {
+    tbody.prepend(row.cloneNode(true));
+    rowsCount++;
+    setCount();
+  }
+
+  if (change === 'removeRow') {
+    row.remove();
+    rowsCount--;
+    setCount();
+  }
+
+  if (change === 'appendColumn') {
+    column.forEach((tr, index) => {
+      tr.prepend(tr.firstElementChild.cloneNode(true));
+    });
+    columnsCount++;
+    setCount();
+  }
+
+  if (change === 'removeColumn') {
+    column.forEach((tr, index) => {
+      tr.firstElementChild.remove();
+    });
+    columnsCount--;
+    setCount();
+  }
+}
+
+function setCount() {
+  if (rowsCount === 10) {
+    appendRow.setAttribute('disabled', '');
+  } else {
+    appendRow.removeAttribute('disabled');
+  }
+
+  if (rowsCount === 2) {
+    removeRow.setAttribute('disabled', '');
+  } else {
+    removeRow.removeAttribute('disabled');
+  }
+
+  if (columnsCount === 10) {
+    appendColumn.setAttribute('disabled', '');
+  } else {
+    appendColumn.removeAttribute('disabled');
+  }
+
+  if (columnsCount === 2) {
+    removeColumn.setAttribute('disabled', '');
+  } else {
+    removeColumn.removeAttribute('disabled');
+  }
+}
